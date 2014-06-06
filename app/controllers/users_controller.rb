0889c07cd7418723f8558ceb808f959b281d1a8f
Joseph-N <josendungu@hotmail.com>
2014-06-06 17:14:04 +0300

    allow admin to access other users profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,21 @@
 class UsersController < ApplicationController
-  before_filter :set_user
-  before_filter :correct_user
+  before_filter :correct_user unless :is_admin?
 
   def show
-    @user = current_user
-  end
-
-  private
-  def set_user
     @user  = User.find(params[:id])
   end
 
   private
   def correct_user
-    unless @user == current_user
+    user = User.find(params[:id])
+    unless user == current_user
       redirect_to root_path, :gflash => {:error => {:title => "Permission Denied",
                                                     :value => "You dont't have permission to access the specified url"
       }}
     end
+  end
+
+  def is_admin?
+    current_user.admin?
   end
 end
